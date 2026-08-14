fc1254c1c7e7143ab7fcf10f076863ee639f4be1
iBin <[]>
2026-08-14 16:26:19 +0800

    build(config): refine typescript, eslint, postcss and package manager settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+/** @type {import("eslint").Linter.Config[]} */
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "data/**",
   ]),
 ]);
 
